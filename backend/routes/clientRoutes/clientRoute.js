2779ccccd87e0c9a5f58d16b63c4cdd83b9766ed
Ozilele <90649604+Ozilele@users.noreply.github.com>
2023-07-29 04:12:13 +0200

    Api client routes

---
+++ backend/routes/clientRoutes/clientRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getHotels } from "../../controllers/admin/hotelsController.js";
+
+const router = express.Router();
+
+router.get("/hotels", getHotels);
+
+export default router;
