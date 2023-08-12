c8274c82b4d07be2f695ddbf1d7989511c965681
Ozilele <90649604+Ozilele@users.noreply.github.com>
2023-08-12 19:22:17 +0200

    Controller for reviews

---
+++ backend/src/controllers/client/reviewController.ts
@@ -0,0 +1,81 @@
+import { Request, Response } from "express";
+import { review_model as ReviewModel } from "../../model/reviewModel.js";
+import { Review, Req } from "../../types/types.js";
+
+export const getReviews = async (req: Request, res: Response) => {
+  const hotelID : string = req.query.hotelID as string || "";
+  const section : number = parseInt(req?.query?.section as string) - 1 || 0;
+  const limitReviews : number = parseInt(req?.query?.limit as string) || 5;
+
+  let totalReviews : number;
+  try {
+    const reviews : Review[] = await ReviewModel.find({ hotel: hotelID }).skip(section * limitReviews).limit(limitReviews).populate({ path: "author", select: "-password -email" });
+    if(reviews) {
+      totalReviews = await ReviewModel.countDocuments({
+        hotel: hotelID
+      });
+      return res.status(201).json({
+        message: "Reviews found",
+        reviews,
+        allReviews: totalReviews
+      }); 
+    } else {
+      return res.status(404).json({
+        message: "Reviews not found"
+      });
+    }
+  }
+  catch(err) {
+    return res.status(500).json({
+      message: "Network error"
+    });
+  }
+}
+
+const didUserPostHotelReview = async (hotelID : string, userID : string) => {
+  const userReviewOfHotel : Review | null = await ReviewModel.findOne({ author: userID, hotel: hotelID }).lean();
+  if(userReviewOfHotel) {
+    return true;
+  }
+  return false;
+}
+
+export const postReview = async (req: Req, res: Response) => {
+  const hotelID : string = req.body.hotelID as string;
+  const rating : number = req.body.rating as number;
+  const content : string = req.body.content as string;
+  const userID : string = req?.user?._id.toString();
+
+  if(hotelID && rating && userID) {
+    const conditionNotToPost = await didUserPostHotelReview(hotelID, userID);
+    if(conditionNotToPost) {
+      return res.status(400).json({
+        message: "User has already reviewed this hotel"
+      });
+    }
+    try {
+      const review : Review = await ReviewModel.create({
+        rating,
+        content,
+        hotel: hotelID,
+        author: userID,
+      });
+      if(review) {
+        return res.status(201).json({
+          message: "Successfully added new review"
+        });
+      } else {
+        return res.status(404).json({
+          message: "New review not added"
+        });
+      }
+    } catch(err) {
+      return res.status(404).json({
+        message: "New review not added"
+      });
+    }
+  }
+  return res.status(500).json({
+    message: "Network error"
+  });
+}
